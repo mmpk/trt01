95e1998f0c07f23c8d09668e2ffc432131fb26c0
mmpk <noreply>
2014-09-17 22:59:56 +0000

    Update component ui/main.reel/main.js

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -19,7 +19,7 @@ exports.Main = Component.specialize(/** @lends Main# */ {
         value: function (event) {
              var flow = this.templateObjects.flow
              var arr = flow.cameraPosition
-             arr[2] = arr[2]-1600
+             arr[2] = arr[2]-1100
              flow.cameraPosition = arr
         }
     }
