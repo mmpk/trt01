28820da0acc691a80a61949fb9cc68db20da12c0
mmpk <noreply>
2014-09-17 23:46:28 +0000

    Update component ui/main.reel/main.js

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -19,9 +19,9 @@ exports.Main = Component.specialize(/** @lends Main# */ {
         value: function (event) {
              var flow = this.templateObjects.flow
              var arr = flow.cameraPosition
-             for(var i=0;i<1000;i++)
+             for(var i=0;i<100;i++)
              {
-             arr[1] = arr[1]+50
+             arr[1] = arr[1]+500
              flow.cameraPosition = arr
              }
         }
@@ -31,9 +31,9 @@ exports.Main = Component.specialize(/** @lends Main# */ {
         value: function (event) {
             var flow = this.templateObjects.flow
             var arr = flow.cameraPosition
-            for(var i=0;i<1000;i++)
+            for(var i=0;i<100;i++)
             {
-                arr[1] = arr[1]-50
+                arr[1] = arr[1]-500
                 flow.cameraPosition = arr
             }
         }
