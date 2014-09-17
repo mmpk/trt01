5daa96ccf3a887539b0b842d4871b2a7157916e7
mmpk <noreply>
2014-09-17 23:44:21 +0000

    Update components

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -25,6 +25,18 @@ exports.Main = Component.specialize(/** @lends Main# */ {
              flow.cameraPosition = arr
              }
         }
+    },
+
+    handleButton2Action: {
+        value: function (event) {
+            var flow = this.templateObjects.flow
+            var arr = flow.cameraPosition
+            for(var i=0;i<1000;i++)
+            {
+                arr[1] = arr[1]-50
+                flow.cameraPosition = arr
+            }
+        }
     }
 
 });
